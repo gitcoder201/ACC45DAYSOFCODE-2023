c7805be21b0448c644c02a16706bf7513c18c250
gitcoder201 <145313501+gitcoder201@users.noreply.github.com>
2023-09-18 10:17:54 +0530

    Add files via upload

---
+++ SmallFactorial.py
@@ -0,0 +1,11 @@
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    result = factorial(n)
+    print(result)
